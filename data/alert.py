b6d106d5797fbd6f35c93546edc037811bea1785
andrei nalivaika <andreinalivaika@andreis-MacBook-Pro-2.local>
2021-10-23 12:04:30 +0300

    refactored TRF structure, added layer framework for current projects objects and layer lib for reusable and custom libs

--- data/alert.py
+++
@@ -1,3 +0,0 @@
-ALERT_PROFILE_UPDATED_VALUE = "Profile was updated successfully"
-ALERT_INVALID_CREDS_VALUE = "Invalid Email or password."
-ALERT_SIGNED_IN_VALUE = "Signed in successfully."
--- data/user.py
+++
@@ -1,3 +0,0 @@
-DICT__USER_CREDENTIALS = {"email": "nav.testsw@gmail.com", "password": "Qwerty123$"}
-USER_CURRENT_NAME = "andrei"
-USER_NEXT_NAME = "andrei1"
