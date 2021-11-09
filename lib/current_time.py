1586b066095b2d9ed2213ba3ae2b9d616cbff39a
andrei nalivaika <andreinalivaika@andreis-MacBook-Pro-2.local>
2021-11-09 19:35:31 +0300

    changed current time py lib

--- lib/current_time.py
+++ lib/current_time.py
@@ -1,6 +1,12 @@
 import datetime
+from robot.api.deco import keyword
 
 
-def get_now():
+@keyword(name="Get Now")
+def get_current_timestamp() -> int:
+    """
+    Get current time and return it
+    :return: current time in ms
+    """
     return int(datetime.datetime.now().timestamp())
 
