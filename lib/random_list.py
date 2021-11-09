9b8f07c4f36ab76b35b7e35b1f28ccdf9326641a
andrei nalivaika <andreinalivaika@andreis-MacBook-Pro-2.local>
2021-11-09 20:20:56 +0300

    refactored random list py method

--- lib/random_list.py
+++ lib/random_list.py
@@ -1,14 +1,15 @@
 import random
+from typing import Optional
 from robot.api.deco import keyword
 
 
 @keyword(name="Get Random Item From List")
-def get_random_item_from_list(list_items, except_item=None):
-    """ Get random item from list and return it
-    :param str list_items: the list of items
-    :param str except_item: the item in the list of items that shouldn't be selected (optional)
+def get_random_item_from_list(list_items: list, except_item: Optional[str] = None) -> str:
+    """
+    Get random item from list and return it
+    :param list_items: the list of items
+    :param except_item: the item in the list of items that shouldn't be selected (optional parameter)
     :return: a random item from current list
-    :rtype: str
     :raises ValueError: thrown if the current parameters are unknown
     """
     try:
