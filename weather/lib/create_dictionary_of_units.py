0735d4d437b10bdc0a44825236c80bf81af3779b
andrei nalivaika <andreinalivaika@andreis-MacBook-Pro-2.local>
2021-11-29 22:09:47 +0300

    refactored create dictionary module

--- weather/lib/create_dictionary_of_units.py
+++ weather/lib/create_dictionary_of_units.py
@@ -11,12 +11,6 @@ def create_dictionary_of_units_params(**kwargs: str) -> dict:
     """
     if "units" not in kwargs:
         kwargs["units"] = "standard"
-    elif kwargs["units"] == "imperial":
-        kwargs["units"] = "imperial"
-    elif kwargs["units"] == "metric":
-        kwargs["units"] = "metric"
-    elif kwargs["units"] != "imperial" or kwargs["units"] != "metric":
+    elif kwargs["units"] != "imperial" and kwargs["units"] != "metric":
         kwargs["units"] = "standard"
-    else:
-        raise ValueError("The current argument isn't allowed")
     return kwargs
